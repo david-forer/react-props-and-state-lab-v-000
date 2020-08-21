c0947f13df025bdf308105259f9ead68f79ebb3b
David Forer <david.j.forer@gmail.com>
2020-08-21 09:08:02 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -52,7 +52,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+              onChangeType={this.handleTypeChange}
+              onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
               <PetBrowser />
